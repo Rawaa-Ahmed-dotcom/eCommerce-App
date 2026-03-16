670a346de30ee6ffe9b4657a6a143148448ec32d
Rawaa Ahmed <rawaaahmed987@gmail.com>
2026-03-16 17:57:40 +0200

    feat: make the login page responsive to the mobile screen

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,10 +3,19 @@ import LoginForm from '../components/forms/LoginForm/LoginForm'
 
 const Login = () => {
   return (
-    <div className="h-full w-full flex fixed flex-col lg:flex-row">
-      <img src = "/public/Section - Left Side_ Hero Image (Visible and fixed on desktop, header on mobile).png" className='object-cover w-[60%] max-h-full'/>
-      <LoginForm/>
-    </div>
+    // 
+    <div className="relative md:fixed w-full h-screen">
+  
+  <img 
+    src="/public/Section - Left Side_ Hero Image (Visible and fixed on desktop, header on mobile).png" 
+    className="absolute inset-0 w-full h-full object-cover md:w-[60%] z-1"
+  />
+  
+ 
+  <div className="absolute bottom-0 left-0 right-0 h-[60%] md:static md:h-full md:w-[40%] md:ml-auto z-10">
+    <LoginForm />
+  </div>
+</div>
   )
 }
 
