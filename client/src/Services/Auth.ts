d3de0b7dbdd5812c163da7d64527bc89545c9ff6
Rawaa Ahmed <rawaaahmed987@gmail.com>
2026-06-12 15:28:11 +0300

    feat: add handleLogin service with axios and error handling

--- client/src/Services/Auth.ts
+++ client/src/Services/Auth.ts
@@ -1,18 +1,42 @@
-import axios from "axios";
-import type { RegisterForm } from "../utils/Types";
+import axios, {  AxiosError } from "axios";
+import type {
+  ApiErrorResponse,
+  LoginForm,
+  LoginResponse,
+  RegisterForm,
+  RegisterResponse,
+} from "../utils/Types";
 
-export const handleRegister = async (data: RegisterForm) => {
+export const handleRegister = async (
+  data: RegisterForm,
+): Promise<RegisterResponse> => {
   try {
-    const res = await axios.post(
+    const res = await axios.post<RegisterResponse>(
       "http://localhost:5000/api/auth/register",
       data,
     );
     console.log(res.data);
     return res.data;
   } catch (err) {
-    const errorMessage: string = err.response?.data?.msg;
-    const fallbackError: string = "UnExpected Error!";
+    const error = err as AxiosError<ApiErrorResponse>;
+    const errorMessage : string = error.response?.data?.msg as string;
+    const fallbackError: string = error.message || "UnExpected Error!";
 
-    throw new Error(errorMessage || fallbackError , {cause : err});
+    throw new Error(errorMessage || fallbackError, { cause: err });
+  }
+};
+export const handleLogin = async (data: LoginForm): Promise<LoginResponse> => {
+  try {
+    const res = await axios.post<LoginResponse>(
+      "http://localhost:5000/api/auth/login",
+      data,
+    );
+    console.log(data);
+    return res.data;
+  } catch (err) {
+    const error = err as AxiosError<ApiErrorResponse>;
+    const errorMessage = error.response?.data?.msg;
+    const fallbackErrorMsg = error?.message || "UnExpected Error!";
+    throw new Error(errorMessage || fallbackErrorMsg, { cause: err });
   }
 };
