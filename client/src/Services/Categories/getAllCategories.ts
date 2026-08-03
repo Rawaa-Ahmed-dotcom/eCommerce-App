cd7751bc3196f6a4f377bc6141f5f71cf29202c6
Rawaa Ahmed <rawaaahmed987@gmail.com>
2026-08-03 19:10:17 +0300

    Fix : fix catch errors in categories services

--- client/src/Services/Categories/getAllCategories.ts
+++ client/src/Services/Categories/getAllCategories.ts
@@ -1,15 +1,16 @@
 import api from "../../api/config";
+import { AxiosError } from "axios";
+import type{ ApiErrorResponse } from "../../utils/Types";
 export const getAllCategories = async () => {
     try {
         const res  = await api.get("/categories");
         const categories = await res.data
         return categories;
-    } catch (err: unknown) {
-        if (err instanceof Error) {
-            console.log(err);
-            throw new Error(err.message, { cause: err });
-        }
-
-        throw new Error("An unknown error occurred while fetching categories.", { cause: err });
+    } catch (err) {
+        const error = err as AxiosError<ApiErrorResponse>;
+            const errorMessage: string = error.response?.data?.msg as string;
+            const fallbackError: string = error.message || "UnExpected Error!";
+        
+            throw new Error(errorMessage || fallbackError, { cause: err });
     }
 }
--- client/src/Services/Categories/getSingleCategory.ts
+++ client/src/Services/Categories/getSingleCategory.ts
@@ -1,5 +1,6 @@
 import api from "../../api/config";
-
+import { AxiosError } from "axios";
+import type{ ApiErrorResponse } from "../../utils/Types";
 export const getSignleCategory = async (slug: string) => {
     if (typeof slug !== "string" || !isNaN(Number(slug)))
         throw new Error("Slug must be string");
@@ -9,6 +10,11 @@ export const getSignleCategory = async (slug: string) => {
     );
     return res.data;
     } catch (err) {
+        const error = err as AxiosError<ApiErrorResponse>;
+            const errorMessage: string = error.response?.data?.msg as string;
+            const fallbackError: string = error.message || "UnExpected Error!";
+        
+            throw new Error(errorMessage || fallbackError, { cause: err });
         throw new Error("Failed to fetch category", { cause: err });
     }
 };
